195fb9a78b09dee2566f5f18c96ce3c1c4e88e00
paradise <pormehriust@gmail.com>
2020-12-02 13:45:36 +0330

    fix Tasks migration

--- database/migrations/2020_12_02_082032_create_tasks_table.php
+++ database/migrations/2020_12_02_082032_create_tasks_table.php
@@ -6,12 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTasksTable extends Migration
 {
+    public $usersTableName = 'users';
+
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on($this->usersTableName)
+                ->cascadeOnDelete();
+
             $table->string('title');
-            $table->boolean('status');
+            $table->boolean('status')->default(false);
 
             $table->timestamps();
         });
